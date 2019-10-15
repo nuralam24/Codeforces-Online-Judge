a154b4a251c7ffa8aa1efaa0b522411209c360e6
Nur Alam <54984550+nuralam24@users.noreply.github.com>
2019-10-15 13:09:13 +0600

    Create main.js

---
+++ Codeforces Round #529 (Div. 3) -- A. Repeating Cipher/main.js
@@ -0,0 +1,17 @@
+//var input = readline()
+var t = parseInt(readline());
+var s = readline();
+ 
+if(s.length == 1) write(s[0]);
+else {
+  var d = 0, m = 0, n = 1;
+  for(var i=0; i<t; i++) {
+      write(s[d]);
+      d += n
+      i += m
+      m++
+      n++ 
+    }
+}
+
+//
