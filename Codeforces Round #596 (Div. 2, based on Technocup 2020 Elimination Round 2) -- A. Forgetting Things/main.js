595e32871cb3ad040f823d41a8ec9d4926750acf
Nur Alam <54984550+nuralam24@users.noreply.github.com>
2019-10-28 12:28:32 +0600

    Create main.js

---
+++ Codeforces Round #596 (Div. 2, based on Technocup 2020 Elimination Round 2) -- A. Forgetting Things/main.js
@@ -0,0 +1,15 @@
+//var input = readline()
+ 
+var ar = readline().split(' ').map(x => parseInt(x));
+var a = ar[0], b = ar[1];
+ 
+ 
+if(a < b  &&  Math.abs(a-b) == 1) {
+    print(a +' '+ b);
+}
+else if(a == 9 && b == 1) {
+    print(9 +' '+ 10);
+}
+else print(a == b ? a+'1' +' '+ b+'2' : -1);
+ 
+//
